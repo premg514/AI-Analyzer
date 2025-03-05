28902600664b0cbc96ab98c71d710fcc88ded028
premg513 <gunduboinaprem@gmail.com>
2025-03-05 09:15:03 +0530

    update

--- server/server.js
+++ server/server.js
@@ -120,5 +120,9 @@ app.post("/generate", verifyToken, async (req, res) => {
     }
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
+if (!PORT) {
+    throw new Error("PORT is not defined in environment variables.");
+}
+
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
